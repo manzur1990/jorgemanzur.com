cae1817b9e4633167b328ee2d419e666eb4030bc
manzur1990 <manzur-1990@hotmail.com>
2022-03-06 17:08:34 -0600

    added directories and css styles

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,15 +1,15 @@
 function App() {
-    return (
-        <>
-            <div>Greeting-Wrapper</div>
-            <div>Intro-Wrapper</div>
-            <div>Nav-Wrapper</div>
-            <div>Left-Column</div>
-            <div>Technologies</div>
-            <div>Get In Touch</div>
-            <div>Footer</div>
-        </>
-    );
+  return (
+    <>
+      <div>Greeting-Wrapper</div>
+      <div>Intro-Wrapper</div>
+      <div>Nav-Wrapper</div>
+      <div>Left-Column</div>
+      <div>Technologies</div>
+      <div>Get In Touch</div>
+      <div>Footer</div>
+    </>
+  );
 }
 
 export default App;
